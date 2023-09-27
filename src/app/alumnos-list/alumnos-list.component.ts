57ff17ffc0124b6bb8ba32a454f98f82c3073656
camilo3900 <camilogv.1629@gmail.com>
2023-09-27 10:26:59 -0500

    Se crea el componente para listado de alumnos

---
+++ src/app/alumnos-list/alumnos-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos-list',
+  templateUrl: './alumnos-list.component.html',
+  styleUrls: ['./alumnos-list.component.scss']
+})
+export class AlumnosListComponent {
+
+}
