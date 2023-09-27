6101da79bc8429dd94832c6d494754cae1788793
camilo3900 <camilogv.1629@gmail.com>
2023-09-27 10:22:11 -0500

    Proyecto inicial

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lista_alumnos';
+  texto = "Hola, buen dia"
+
+
+  /* Se crea un array de alumnos de tipo Alumno */
+
+  
+}
